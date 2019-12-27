2ac9c8e7bb1fe78a1c1230e64657d5cdbc5ff490
Skyler Martyn <skyler.martyn@gmail.com>
2019-12-27 16:40:17 +0100

    Move some styling to CSS file, add color change for drawing effect

--- main.js
+++ main.js
@@ -2,17 +2,11 @@ const html = document.querySelector('html');
 html.style.height = '1000px';
 html.style.width = '1000px';
 
-const container = document.querySelector('#container');
+const drawingBoard = document.querySelector('#drawing-board');
 
 const body = document.querySelector('body');
 body.style.height = '1000px';
 body.style.width = '1000px';
-
-const gridBody = document.createElement('div');
-gridBody.style.display = 'grid';
-gridBody.style.gridTemplateColumns = '100px'.repeat(16);
-gridBody.style.gridTemplateRows = '100px'.repeat(16);
-gridBody.style.gridGap = '5px';
 
 const gridCells = [];
 
@@ -22,10 +16,12 @@ for (let row = 1; row < 17; row++) {
         newCell.style.gridRow = `${row} / ${row + 1}`
         newCell.style.gridColumn = `${col} / ${col + 1}`
 
-        newCell.style.backgroundColor = 'pink';
-        newCell.style.width = '80px';
-        newCell.style.height = '80px';
-        newCell.style.margin = 'auto';
+        newCell.classList.add('square');
+        newCell.addEventListener('mouseover', (e) => {
+            if (e.target.style.backgroundColor !== 'black') {
+                e.target.style.backgroundColor = 'black';
+            }
+        });
         gridCells.push(newCell);
     }
 }
@@ -33,8 +29,7 @@ for (let row = 1; row < 17; row++) {
 console.log(gridCells.length);
 
 for (x = 0; x < gridCells.length; x++) {
-    gridBody.appendChild(gridCells[x]);
+    drawingBoard.appendChild(gridCells[x]);
 }
 
-container.appendChild(gridBody);
 console.log('wahoo');
