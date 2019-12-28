edd859d90d5bd45915f2aa5439f10b9bb2c193e5
Skyler Martyn <skyler.martyn@gmail.com>
2019-12-28 11:32:45 +0100

    Fix reset button, abstract gridCell construction

--- main.js
+++ main.js
@@ -1,27 +1,57 @@
+// Number of squares per side
+const gridFid = 16;
+// Width of each square
+const gridSide = Math.floor(960 / gridFid);
+// Grid cells array
+const gridCells = [];
+
+
+// Variables pre-selected for sections of the page
+const header = document.querySelector('header');
+const body = document.querySelector('body');
+const footer = document.querySelector('footer');
+const drawingBoard = document.querySelector('#drawing-board');
 const html = document.querySelector('html');
+
+
+// Reset button
+const resetButton = document.createElement('button');
+resetButton.textContent = 'Reset Board';
+resetButton.addEventListener('click', () => {
+    for (x = 0; x < drawingBoard.children.length; x++) {
+        drawingBoard.children[x].style.backgroundColor = 'pink';
+    }
+})
+header.appendChild(resetButton);
+
+
+
 html.style.height = '1000px';
 html.style.width = '1000px';
 
-const drawingBoard = document.querySelector('#drawing-board');
 
-const body = document.querySelector('body');
+drawingBoard.style.size = '960px 960px';
+
+
 body.style.height = '1000px';
 body.style.width = '1000px';
 
-const gridCells = [];
 
-for (let row = 1; row < 17; row++) {
-    for (let col = 1; col < 17; col++) {
+
+for (let row = 1; row < 16 + 1; row++) {
+    for (let col = 1; col < 16 + 1; col++) {
         const newCell = document.createElement('div');
         newCell.style.gridRow = `${row} / ${row + 1}`
         newCell.style.gridColumn = `${col} / ${col + 1}`
 
-        newCell.classList.add('square');
+        
+        newCell.style.size = `${gridSide} ${gridSide}`;
+        newCell.style.backgroundColor = 'pink';
         newCell.addEventListener('mouseover', (e) => {
             if (e.target.style.backgroundColor !== 'black') {
                 e.target.style.backgroundColor = 'black';
             }
-        });
+        }); 
         gridCells.push(newCell);
     }
 }
@@ -31,5 +61,7 @@ console.log(gridCells.length);
 for (x = 0; x < gridCells.length; x++) {
     drawingBoard.appendChild(gridCells[x]);
 }
+
+
 
 console.log('wahoo');
